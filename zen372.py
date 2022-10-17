e04f31666ad5df23edef0e68ce4c8abd5b8778d7
GerardoKim22 <gerardo.kim@uabc.edu.mx>
2022-10-16 22:01:54 -0700

    Creacion de main.py y git ignore

---
+++ zen372.py
@@ -0,0 +1,16 @@
+# Comentario 1 linea
+
+"""
+Comentario multilinea
+1 Linea
+2 Lineas
+3 ......
+"""
+
+print("Latosa1: Nose")
+print("Latosa2: Mas vale pedir pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme bla bla bla")
+print("Marco2: Fuga!!!")
+print("Derek: Fuga!!! x2")
+
+barba = "Algo que sorpresa!!!"
